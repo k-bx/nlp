a0a8bb88442c03a5af9a7f519eca8403ac879a31
Kostiantyn Rybnikov <k-bx@k-bx.com>
2018-03-08 12:18:47 +0200

    Implement first working rule

--- taikyoku-shogi/src/Main.hs
+++ taikyoku-shogi/src/Main.hs
@@ -7,14 +7,26 @@
 module Main where
 
 import Data.Foldable (foldl')
+import qualified Data.HashMap.Strict as H
+import Data.HashMap.Strict (HashMap)
 import Data.Semigroup
 import Data.String (IsString(..))
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Data.Tuple (swap)
+import Debug.Trace
 import Formatting ((%), (%.), sformat)
 import Formatting.ShortFormatters (r, st)
 import GHC.Records
+import GHC.Stack
+import Text.Groom (groom)
+
+impossible :: HasCallStack => a
+impossible = error "不可能！！！"
+
+impossibleS :: (Show b, HasCallStack) => b -> a
+impossibleS b = error ("不可能！！！\n" ++ groom b)
 
 data JapaneseName = JapaneseName
   { name :: Text
@@ -100,7 +112,61 @@ rules :: [Rule]
 rules = [Rule "Known translations" ruleKnownTranslations]
 
 ruleKnownTranslations :: Table -> Table
-ruleKnownTranslations tbl = tbl
+ruleKnownTranslations tbl =
+  -- trace ">> ruleKnownTranslations" $
+  -- trace (T.unpack (renderTable tbl)) $
+  -- trace ">>" $
+  let jpToEn :: [(Text, Text)]
+      jpToEn = foldl' jpToEnF [] tbl
+      jpToEnF :: [(Text, Text)] -> Promotion -> [(Text, Text)]
+      jpToEnF dict Promotion {..} = dict ++ getTrans from ++ getTrans to
+      getTrans :: NamePair -> [(Text, Text)]
+      getTrans NPair {..} =
+        case (jpName, enName) of
+          (Nothing, _) -> []
+          (_, Nothing) -> []
+          ((Just jpName), (Just enName)) ->
+            [(getField @"name" jpName, getField @"name" enName)]
+      jpToEnHm :: HashMap Text Text
+      jpToEnHm = H.fromListWith failIfDifferent jpToEn
+      failIfDifferent from to =
+        if from /= to
+          then impossibleS jpToEn
+          else from
+      enToJpHm :: HashMap Text Text
+      enToJpHm =
+        let swapped = map swap jpToEn
+        in H.fromListWith failIfDifferent swapped
+      fillPromotion :: Promotion -> Promotion
+      fillPromotion Promotion {..} = Promotion (fillNP from) (fillNP to)
+      fillNP :: NamePair -> NamePair
+      fillNP np@NPair {..} =
+        case (jpName, enName) of
+          (Just jpNameJust, Just enNameJust) ->
+            if H.lookup (getField @"name" jpNameJust) jpToEnHm ==
+               Just (getField @"name" enNameJust)
+              then if H.lookup (getField @"name" enNameJust) enToJpHm ==
+                      Just (getField @"name" jpNameJust)
+                     then np
+                     else impossible
+              else impossible
+          (Just jpNameJust, Nothing) ->
+            np
+            { enName =
+                fmap
+                  EnglishName
+                  (H.lookup (getField @"name" jpNameJust) jpToEnHm)
+            }
+          (Nothing, Just enNameJust) ->
+            np
+            { jpName =
+                fmap
+                  JapaneseName
+                  (H.lookup (getField @"name" enNameJust) enToJpHm)
+            }
+          (_, _) -> np
+      resTbl = map fillPromotion tbl
+  in resTbl
 
 type Diff = [PairChange]
 
@@ -148,7 +214,6 @@ solve table =
 main :: IO ()
 main = do
   T.putStrLn (renderTable initialTable)
-  putStrLn ""
   putStrLn ""
   let (result, log) = solve initialTable
   mapM T.putStrLn log
