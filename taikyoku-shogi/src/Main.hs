91778623bd71cbdecd4fbffc5281f7fd8dcf2e15
Kostiantyn Rybnikov <k-bx@k-bx.com>
2018-03-08 17:39:53 +0200

    Parse weapons

--- taikyoku-shogi/src/Main.hs
+++ taikyoku-shogi/src/Main.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -8,22 +9,26 @@
 
 module Main where
 
-import Control.Lens ((&), (.~), (^.))
+--import Control.Lens ((%~), (&), (.~), (^.), _Just)
+--import Debug.Trace
+import Control.Lens ((^.))
 import Data.Foldable (foldl')
 import Data.Generics.Product (field)
 import qualified Data.HashMap.Strict as H
 import Data.HashMap.Strict (HashMap)
+import qualified Data.List as List
+import Data.Maybe (catMaybes, listToMaybe)
 import Data.Semigroup
 import Data.String (IsString(..))
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Data.Tuple (swap)
-import Debug.Trace
 import Formatting ((%), (%.), sformat)
 import Formatting.ShortFormatters (r, st)
 import GHC.Generics (Generic)
 import GHC.Stack
+import Safe
 import Text.Groom (groom)
 
 impossible :: HasCallStack => a
@@ -33,22 +38,38 @@ impossibleS :: (Show b, HasCallStack) => b -> a
 impossibleS b = error ("不可能！！！\n" ++ groom b)
 
 data JapaneseName = JapaneseName
-  { name :: Text
+  { name :: Maybe Text
   } deriving (Eq, Show, Generic)
+
+mkJapaneseName :: Text -> JapaneseName
+mkJapaneseName = JapaneseName . Just
 
 instance IsString JapaneseName where
-  fromString = JapaneseName . T.pack
+  fromString s = mkJapaneseName (T.pack s)
 
 data EnglishName = EnglishName
-  { name :: Text
+  { name :: Maybe Text
+  , weapon :: Maybe Weapon
   } deriving (Eq, Show, Generic)
 
+mkEnglishName :: Text -> EnglishName
+mkEnglishName x = EnglishName (Just x) Nothing
+
+data Weapon
+  = Sword
+  | Bow
+  | Crossbow
+  deriving (Eq, Show, Generic, Read)
+
+parseWeapon :: Text -> Maybe Weapon
+parseWeapon = readMay . T.unpack
+
 instance IsString EnglishName where
-  fromString = EnglishName . T.pack
+  fromString s = mkEnglishName (T.pack s)
 
 data NamePair = NPair
-  { jpName :: Maybe JapaneseName
-  , enName :: Maybe EnglishName
+  { jpName :: JapaneseName
+  , enName :: EnglishName
   } deriving (Eq, Show, Generic)
 
 data Promotion = Promotion
@@ -72,16 +93,21 @@ renderPair :: NamePair -> Text
 renderPair (NPair {..}) =
   sformat
     ((r 9 ' ' %. st) % " " % (r 18 ' ' %. st))
-    (renderMJpName jpName)
-    (renderMEnName enName)
+    (renderJpName jpName)
+    (renderEnName enName)
 
-renderMJpName :: Maybe JapaneseName -> Text
-renderMJpName Nothing = "_____"
-renderMJpName (Just (JapaneseName {..})) = name
+renderJpName :: JapaneseName -> Text
+renderJpName JapaneseName {..} = renderName name
 
-renderMEnName :: Maybe EnglishName -> Text
-renderMEnName Nothing = "_____"
-renderMEnName (Just (EnglishName {..})) = name
+renderEnName :: EnglishName -> Text
+renderEnName EnglishName {..} = features <> " " <> renderName name
+  where
+    features = T.concat (List.intersperse " " (catMaybes [fmap sh weapon]))
+    sh x = "[" <> tshow x <> "]"
+
+renderName :: IsString p => Maybe p -> p
+renderName Nothing = "_____"
+renderName (Just t) = t
 
 -- | Just to make things nicer to write
 instance IsString a => IsString (Maybe a) where
@@ -89,33 +115,47 @@ instance IsString a => IsString (Maybe a) where
 
 initialTable :: Table
 initialTable =
-  [ Promotion (NPair Nothing "Running Stag") (NPair "honroku" Nothing)
-  , Promotion (NPair Nothing Nothing) (NPair "tōshō" "Sword General")
-  , Promotion (NPair "gyūhei" "Ox Soldier") (NPair Nothing Nothing)
-  , Promotion (NPair Nothing "Right General") (NPair "ugun" Nothing)
-  , Promotion (NPair Nothing "Ox General") (NPair "hongyū" Nothing)
-  , Promotion (NPair "kyūhei" "Bow Soldier") (NPair Nothing "Bow General")
-  , Promotion (NPair "kisho" "Wood General") (NPair Nothing Nothing)
-  , Promotion (NPair "dohei" "Crossbow Soldier") (NPair Nothing Nothing)
-  , Promotion (NPair Nothing Nothing) (NPair Nothing "Dashing Horse")
-  , Promotion (NPair Nothing "Left Chariot") (NPair Nothing Nothing)
-  , Promotion (NPair Nothing "Horse Soldier") (NPair "sōba" Nothing)
-  , Promotion (NPair "sōyū" "Running Bear") (NPair Nothing "Dashing Bear")
-  , Promotion (NPair "tonshō" "Pig General") (NPair "honton" Nothing)
-  , Promotion (NPair "tesshō" Nothing) (NPair "hakuzō" "White Elephant")
-  , Promotion (NPair Nothing Nothing) (NPair "sagun" "Left Army")
-  , Promotion (NPair "usha" Nothing) (NPair "utessha" "Right Iron Chariot")
-  , Promotion (NPair Nothing Nothing) (NPair "honrō" "Dashing Wolf")
-  , Promotion (NPair "sekishō" "Stone General") (NPair Nothing "White Elephant")
+  [ Promotion (NPair jpNone "Running Stag") (NPair "honroku" enNone)
+  , Promotion (NPair jpNone enNone) (NPair "tōshō" "Sword General")
+  , Promotion (NPair "gyūhei" "Ox Soldier") (NPair jpNone enNone)
+  , Promotion (NPair jpNone "Right General") (NPair "ugun" enNone)
+  , Promotion (NPair jpNone "Ox General") (NPair "hongyū" enNone)
+  , Promotion (NPair "kyūhei" "Bow Soldier") (NPair jpNone "Bow General")
+  , Promotion (NPair "kisho" "Wood General") (NPair jpNone enNone)
+  , Promotion (NPair "dohei" "Crossbow Soldier") (NPair jpNone enNone)
+  , Promotion (NPair jpNone enNone) (NPair jpNone "Dashing Horse")
+  , Promotion (NPair jpNone "Left Chariot") (NPair jpNone enNone)
+  , Promotion (NPair jpNone "Horse Soldier") (NPair "sōba" enNone)
+  , Promotion (NPair "sōyū" "Running Bear") (NPair jpNone "Dashing Bear")
+  , Promotion (NPair "tonshō" "Pig General") (NPair "honton" enNone)
+  , Promotion (NPair "tesshō" enNone) (NPair "hakuzō" "White Elephant")
+  , Promotion (NPair jpNone enNone) (NPair "sagun" "Left Army")
+  , Promotion (NPair "usha" enNone) (NPair "utessha" "Right Iron Chariot")
+  , Promotion (NPair jpNone enNone) (NPair "honrō" "Dashing Wolf")
+  , Promotion (NPair "sekishō" "Stone General") (NPair jpNone "White Elephant")
   ]
+  where
+    jpNone = JapaneseName Nothing
+    enNone = EnglishName Nothing Nothing
 
 data Rule = Rule
   { name :: Text
   , fn :: Table -> Table
   } deriving (Generic)
 
+type Diff = [PairChange]
+
+data PairChange = PairChange
+  { from :: Promotion
+  , to :: Promotion
+  , rule :: Rule
+  }
+
 rules :: [Rule]
-rules = [Rule "Known translations" ruleKnownTranslations]
+rules =
+  [ Rule "Known translations" ruleKnownTranslations
+  , Rule "Parse features" ruleParseFeatures
+  ]
 
 ruleKnownTranslations :: Table -> Table
 ruleKnownTranslations tbl
@@ -129,10 +169,10 @@ ruleKnownTranslations tbl
       jpToEnF dict Promotion {..} = dict ++ getTrans from ++ getTrans to
       getTrans :: NamePair -> [(Text, Text)]
       getTrans NPair {..} =
-        case (jpName, enName) of
+        case (jpName ^. field @"name", enName ^. field @"name") of
           (Nothing, _) -> []
           (_, Nothing) -> []
-          ((Just jp), (Just en)) -> [(jp ^. field @"name", en ^. field @"name")]
+          ((Just jp), (Just en)) -> [(jp, en)]
       jpToEnHm :: HashMap Text Text
       jpToEnHm = H.fromListWith failIfDifferent jpToEn
       failIfDifferent from to =
@@ -146,39 +186,43 @@ ruleKnownTranslations tbl
       fillPromotion :: Promotion -> Promotion
       fillPromotion Promotion {..} = Promotion (fillNP from) (fillNP to)
       fillNP :: NamePair -> NamePair
-      fillNP np@NPair {..} = fillNP' np jpName enName
+      fillNP np@NPair {..} =
+        fillNP' np (jpName ^. field @"name") (enName ^. field @"name")
       fillNP' np (Just jp) (Just en)
         | isInBothDicts jp en = np
       fillNP' _ (Just _) (Just _)
         | otherwise = impossible
       fillNP' np (Just jp) Nothing =
-        let newName = fmap EnglishName (H.lookup (jp ^. field @"name") jpToEnHm)
-        in np {enName = newName}
+        case H.lookup jp jpToEnHm of
+          Just en -> np {enName = mkEnglishName en}
+          Nothing -> np
       fillNP' np Nothing (Just en) =
-        let newName =
-              fmap JapaneseName (H.lookup (en ^. field @"name") enToJpHm)
-        in np {jpName = newName}
+        case H.lookup en enToJpHm of
+          Just jp -> np {jpName = mkJapaneseName jp}
+          Nothing -> np
       fillNP' np _ _ = np
-      isInBothDicts :: JapaneseName -> EnglishName -> Bool
+      isInBothDicts :: Text -> Text -> Bool
       isInBothDicts jp en = isInJpDict jp en && isInEnDict en jp
-      isInJpDict :: JapaneseName -> EnglishName -> Bool
-      isInJpDict jp en =
-        H.lookup (jp ^. field @"name") jpToEnHm == Just (en ^. field @"name")
-      isInEnDict :: EnglishName -> JapaneseName -> Bool
-      isInEnDict en jpNameJust =
-        H.lookup (en ^. field @"name") enToJpHm ==
-        Just (jpNameJust ^. field @"name")
+      isInJpDict :: Text -> Text -> Bool
+      isInJpDict jp en = H.lookup jp jpToEnHm == Just en
+      isInEnDict :: Text -> Text -> Bool
+      isInEnDict en jp = H.lookup (en) enToJpHm == Just jp
       resTbl = map fillPromotion tbl
   in resTbl
 
-type Diff = [PairChange]
+-- TODO: replace listToMaybe with impossible upon multiple different candidates
+ruleParseFeatures :: Table -> Table
+ruleParseFeatures = map parsePromotion
+  where
+    parsePromotion Promotion {..} = Promotion (parseNP from) (parseNP to)
+    parseNP NPair {..} = NPair (parseJP jpName) (parseEN enName)
+    parseJP = id
+    parseEN en@EnglishName {name = Nothing, ..} = en
+    parseEN en@EnglishName {name = Just n, ..} =
+      let wrds = T.words n
+      in en {weapon = listToMaybe (catMaybes (map parseWeapon wrds))}
 
-data PairChange = PairChange
-  { from :: Promotion
-  , to :: Promotion
-  , rule :: Rule
-  }
-
+-- TODO: optimize concatenation of diffs
 solve :: Table -> (Table, [Text])
 solve table =
   let (table2, diffs2) = foldl' applyRule (table, []) rules
@@ -187,7 +231,6 @@ solve table =
        _ ->
          let (table3, diffs3) = solve table2
          in (table3, renderDiff diffs2 ++ diffs3)
-    -- TODO: optimize concatenation of diffs
   where
     applyRule :: (Table, Diff) -> Rule -> (Table, Diff)
     applyRule (tbl, diff1) (rule@Rule {..}) =
@@ -213,6 +256,9 @@ solve table =
       , "  " <> renderPromotion to
       , ""
       ]
+
+tshow :: Show a => a -> Text
+tshow = T.pack . show
 
 main :: IO ()
 main = do
