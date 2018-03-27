c89287fb1a8df4b143ea5e1490bb6f942747203b
Kostiantyn Rybnikov <k-bx@k-bx.com>
2018-03-27 19:51:09 +0300

    Parse in CoreNLP

--- factcheck/src/Main.hs
+++ factcheck/src/Main.hs
@@ -11,11 +11,14 @@ import Data.Maybe
 import qualified Data.String.Class as S
 import qualified Data.Text as T
 import Data.Text (Text)
-import qualified Data.Text.IO as T
-import qualified Network.Wreq as W
-import Safe hiding (at)
 import Text.Groom
+import qualified Data.Text.IO as T
+import NLP.CoreNLP
+import qualified Network.Wreq as W
+import Safe
+import System.Environment
 import Text.HTML.Scalpel.Core
+import qualified Text.HTML.Scalpel.Core as SC
 import Text.Pandoc
 import Text.Regex.PCRE.Heavy
 
@@ -27,8 +30,8 @@ data Book = Book
 parseBooks :: Scraper Text [Book]
 parseBooks = do
   chroots ("tr" @: ["itemtype" @= "http://schema.org/Book"]) $ do
-    title <- chroot ("a" @: [hasClass "bookTitle"]) (text "span")
-    pubYearRaw <- text $ "td" @: ["width" @= "100%"]
+    title <- chroot ("a" @: [hasClass "bookTitle"]) (SC.text "span")
+    pubYearRaw <- SC.text $ "td" @: ["width" @= "100%"]
     let mRes = scan [re|published[^0-9]*([0-9]+)|] pubYearRaw
     let mpubYear = mRes & headMay <&> snd >>= headMay <&> T.unpack >>= readMay
     return Book {..}
@@ -51,19 +54,34 @@ downloadWikiBody = do
       "https://en.wikipedia.org/w/api.php?action=query&titles=Sam_Harris&prop=revisions&rvprop=content&format=json"
   return $ fromMaybe "" $ getWikiBody (r ^. W.responseBody . to S.toText)
 
+factCheck :: [ParsedDocument] -> [Book] -> IO ()
+factCheck = undefined
+
 main :: IO ()
 main = do
-  t <- T.readFile "data/goodreads_samharris.html"
-  --t <- downloadGoodreads
-  putStrLn $ groom $ scrapeStringLike t parseBooks
-  -- mwikiBody <- downloadWikiBody
-  mwikiBody <- getWikiBody <$> T.readFile "data/wiki_api_response.json"
-  let wikiBody = fromMaybe (error "failed to get wiki body") mwikiBody
-  let wikiTxt =
-        (either
-           (error . show)
-           id
-           (runPure
-              (do mr <- readMediaWiki def wikiBody
-                  writePlain def {writerWrapText = WrapNone} mr)))
-  T.putStrLn wikiTxt
+  args <- getArgs
+  case args of
+    [corenlp, cache] -> do
+      t <- T.readFile "data/goodreads_samharris.html"
+      --t <- downloadGoodreads
+      let books = scrapeStringLike t parseBooks
+      -- mwikiBody <- downloadWikiBody
+      mwikiBody <- getWikiBody <$> T.readFile "data/wiki_api_response.json"
+      let wikiBody = fromMaybe (error "failed to get wiki body") mwikiBody
+      let wikiTxt =
+            (either
+               (error . show)
+               Prelude.id
+               (runPure
+                  (do mr <- readMediaWiki def wikiBody
+                      writePlain def {writerWrapText = WrapNone} mr)))
+      docs <-
+        launchCoreNLP
+          corenlp
+          def {numWorkers = 1, cacheDb = Just cache}
+          (T.lines wikiTxt)
+      putStrLn $ groom $ docs
+      factCheck docs (fromMaybe [] books)
+      return ()
+    _ ->
+      putStrLn "Usage: stack exec factcheck -- /path/to/corenlp /path/to/cache"
