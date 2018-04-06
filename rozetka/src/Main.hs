12108994fdd4aeda75761004c523761ec7765466
Kostiantyn Rybnikov <k-bx@k-bx.com>
2018-04-06 23:34:32 +0300

    Scraping seems to work

--- rozetka/src/Main.hs
+++ rozetka/src/Main.hs
@@ -1,10 +1,15 @@
 module Main where
 
 import qualified Data.Aeson as J
+import qualified Data.String.Class as S
 import qualified Data.Text as T
 import qualified Network.Wreq as W
 import Options.Applicative.Simple
+import qualified Prelude
 import RIO
+import qualified RIO.HashMap as Hash
+import Text.HTML.Scalpel.Core
+import qualified Data.Text.IO as T
 
 jsonOpts :: J.Options
 jsonOpts = J.defaultOptions {J.fieldLabelModifier = J.camelTo2 '_'}
@@ -28,7 +33,18 @@ data PageData = PageData
   } deriving (Eq, Show, Generic)
 
 instance J.FromJSON PageData where
-  parseJSON = J.genericParseJSON jsonOpts
+  parseJSON =
+    J.withObject "PageData" $ \hm ->
+      let Just d = Hash.lookup "data" hm
+      in J.genericParseJSON jsonOpts d
+
+data Comment = Comment
+  { cScore :: Int
+  , cText :: Text
+  } deriving (Show, Eq, Generic)
+
+instance J.ToJSON Comment where
+  toJSON = J.genericToJSON jsonOpts
 
 getPageData :: CategoryId -> PageId -> RIO App PageData
 getPageData catId pageId = do
@@ -38,6 +54,7 @@ getPageData catId pageId = do
         "&page=" <>
         show pageId <>
         "&lang=ua"
+  logDebug $ display $ "Downloading: " <> tshow url
   r <- liftIO $ W.asJSON =<< W.get url
   threadDelay 1000000
   return (r ^. W.responseBody)
@@ -51,19 +68,82 @@ getProductIds catId = do
       return $ ids pdata
   return (ids p1data ++ concat restIds)
 
-getComments :: ProductId -> RIO App ()
-getComments productId = do
-  let url = "https://rozetka.com.ua/ua/" <> show productId <> "/p" <> show productId <> "/tab=comments/"
-  undefined
+parseComments :: Scraper Text [Comment]
+parseComments =
+  chroots ("article" @: [hasClass "pp-review-i"]) $ do
+    cScore <-
+      fmap
+        (Prelude.read . T.unpack)
+        (attr "content" ("span" @: [hasClass "g-rating-stars-i"]))
+    cTextRaw <- text ("div" @: [hasClass "pp-review-text-i"])
+    let cText = T.strip cTextRaw
+    return Comment {..}
+
+testParseComments01 :: IO ()
+testParseComments01 = do
+  t <- readFileUtf8 "data/comments_multi_page.html"
+  let (Just coms) = scrapeStringLike t parseComments
+  forM_ coms $ \Comment{..} -> do
+    Prelude.putStrLn $ "Rating: " <> show cScore
+    T.putStrLn $ "Comment: \n" <> cText
+
+parseNumCommentPages :: Scraper Text Int
+parseNumCommentPages = do
+  pageNumTexts <-
+    chroots ("li" @: [hasClass "paginator-catalog-l-i"]) (text "span")
+  let pageNums = map (Prelude.read . S.toString) pageNumTexts
+  case pageNums of
+    [] -> return 1
+    _ -> return (Prelude.last pageNums)
+
+testParseNumCommentPages01 :: IO ()
+testParseNumCommentPages01 = do
+  t <- readFileUtf8 "data/comments_multi_page.html"
+  let (Just numPages) = scrapeStringLike t parseNumCommentPages
+  Prelude.print numPages
+
+getComments :: ProductId -> PageId -> RIO App [Comment]
+getComments productId page = do
+  let url =
+        "https://rozetka.com.ua/ua/" <> show productId <> "/p" <> show productId <>
+        "/tab=comments;page=" <>
+        show page <>
+        "/"
+  logDebug $ display $ "Downloading: " <> tshow url
+  r <- liftIO $ W.get url
+  threadDelay 1000000
+  let comPages =
+        fromMaybe 1 $
+        scrapeStringLike
+          (r ^. W.responseBody . to S.toText)
+          parseNumCommentPages
+  let comsCurrPage =
+        fromMaybe [] $
+        scrapeStringLike (r ^. W.responseBody . to S.toText) parseComments
+  if comPages <= page
+    then return comsCurrPage
+    else do
+      comsRest <- getComments productId (page + 1)
+      return (comsCurrPage ++ comsRest)
+  -- forM [comPages
+  -- comsRest <-
+  --   concat <$>
+  --   if comPages <= 2
+  --     then return []
+  --     else (forM ([3 .. comPages]) (getComments productId))
+  -- return (comsp1 <> comsRest)
 
 app :: RIO App ()
 app = do
   let catId = 4637799 :: CategoryId
   logDebug $ display $ "Grabbing the category id: " <> tshow catId
   productIds <- getProductIds catId
-  prodWithComms <- forM productIds $ \pId -> do
-    cs <- getComments pId
-    return (pId, cs)
+  prodWithComms <-
+    forM productIds $ \pId -> do
+      cs <- getComments pId 1
+      return (pId, cs)
+  writeFileUtf8 "data/comments.json" (S.toText (J.encode prodWithComms))
+  logDebug $ "Wrote data in data/comments.json"
   return ()
 
 main :: IO ()
@@ -73,7 +153,7 @@ main = do
       "0.1.0.0"
       "rozetka reviews"
       "rozetka review extractor"
-      (flag False True (long "verbose"))
+      (flag True False (long "no-verbose"))
       empty
   lo <- logOptionsHandle stdout verbose
   withLogFunc lo $ \appLogFunc -> runRIO App {..} $ do app
