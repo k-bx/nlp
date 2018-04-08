d2cd5d664653c9142522b2ba545bb396d32ddd55
Kostiantyn Rybnikov <k-bx@k-bx.com>
2018-04-08 22:15:20 +0300

    Some naive optimizations

--- naiveml/src/NaiveML/MultinomialNaiveBayes.hs
+++ naiveml/src/NaiveML/MultinomialNaiveBayes.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE DeriveAnyClass #-}
 
 -- | See the "Multinomial Naive Bayes for Text Categorization
 -- Revisited" paper (present in the "data" dir of the repo). See the
@@ -20,7 +20,11 @@ module NaiveML.MultinomialNaiveBayes
 import Control.Newtype
 import qualified Data.Aeson as J
 import Data.Function (on)
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as H
+import Data.Hashable
 import Data.List (nub, sortBy)
+import Data.Maybe (fromMaybe)
 import Data.Maybe (fromJust)
 import qualified Data.String.Class as S
 import qualified Data.Text.IO as T
@@ -34,29 +38,45 @@ data Document cls word = Document
 instance (J.FromJSON cls, J.FromJSON word) =>
          J.FromJSON (Document cls word)
 
+data EfficiencyCache cls word = EfficiencyCache
+  { docsPerClass :: HashMap cls [Document cls word]
+  , docsWithWordNum :: HashMap word Int
+  , docsTotalNumber :: Double
+  , effVocabularySize :: Double
+  } deriving (Show, Eq, Generic)
+
 -- | Globals used in formulas
 data Env cls word = Env
   { envClasses :: [cls]
   , envDocs :: [Document cls word]
   , envVocabulary :: [word]
+  , envEfficiencyCache :: EfficiencyCache cls word -- ^ added purely for lookup efficiency
   } deriving (Show, Eq, Generic)
-
-instance (J.FromJSON cls, J.FromJSON word) => J.FromJSON (Env cls word)
 
 dlength :: [a] -> Double
 dlength = fromIntegral . length
 
-docsInClass :: Eq cls => Env cls word -> cls -> [Document cls word]
-docsInClass env c = filter (\d -> c `elem` docClasses d) (envDocs env)
+docsInClass ::
+     (Eq cls, Hashable cls) => Env cls word -> cls -> [Document cls word]
+docsInClass env c =
+  fromMaybe [] (H.lookup c (docsPerClass (envEfficiencyCache env)))
+  where
+    _noCacheImp = filter (\d -> c `elem` docClasses d) (envDocs env)
 
-docsWithWord :: Eq word => Env cls word -> word -> [Document cls word]
-docsWithWord env w = filter (\d -> w `elem` docWords d) (envDocs env)
+countDocsWithWord :: (Eq word, Hashable word) => Env cls word -> word -> Double
+countDocsWithWord env w =
+  fromIntegral $
+  fromMaybe 0 (H.lookup w (docsWithWordNum (envEfficiencyCache env)))
+  where
+    _noCacheImp = dlength (filter (\d -> w `elem` docWords d) (envDocs env))
 
 totalNumberOfDocs :: Env cls word -> Double
-totalNumberOfDocs env = dlength (envDocs env)
+totalNumberOfDocs env = docsTotalNumber (envEfficiencyCache env)
+  where
+    _noCacheImp = dlength (envDocs env)
 
 -- | Pr(c)
-classPrior :: Eq cls => Env cls word -> cls -> Double
+classPrior :: (Eq cls, Hashable cls) => Env cls word -> cls -> Double
 classPrior env c = dlength (docsInClass env c) / totalNumberOfDocs env
 
 -- | f_n_i
@@ -65,17 +85,19 @@ countWordInDoc w d = dlength (filter (== w) (docWords d))
 
 -- | F_x_c
 wordCountInTrainingDocsClass ::
-     (Eq word, Eq cls) => Env cls word -> cls -> word -> Double
+     (Eq word, Eq cls, Hashable cls) => Env cls word -> cls -> word -> Double
 wordCountInTrainingDocsClass env c w =
   sum (map (countWordInDoc w) (docsInClass env c))
 
 -- | N
 vocabularySize :: Env cls word -> Double
-vocabularySize env = dlength (envVocabulary env)
+vocabularySize env = effVocabularySize (envEfficiencyCache env)
+  where
+    _noCacheImp = dlength (envVocabulary env)
 
 -- | Pr(w_n|c)
 wordGivenClassProbability ::
-     (Eq word, Eq cls) => Env cls word -> word -> cls -> Double
+     (Eq word, Eq cls, Hashable cls) => Env cls word -> word -> cls -> Double
 wordGivenClassProbability env w c =
   (1 + wordCountInTrainingDocsClass env c w) /
   (vocabularySize env +
@@ -83,20 +105,28 @@ wordGivenClassProbability env w c =
 
 -- | Pr(t_i|c)
 documentProbability ::
-     (Eq word, Eq cls) => Env cls word -> Document cls word -> cls -> Double
+     (Eq word, Eq cls, Hashable cls)
+  => Env cls word
+  -> Document cls word
+  -> cls
+  -> Double
 documentProbability env d c = product (map f (envVocabulary env))
   where
     f w = (wordGivenClassProbability env w c) ** (countWordInDoc w d)
 
 -- | Pr(t_i)
 normalizationFactor ::
-     (Eq word, Eq cls) => Env cls word -> Document cls word -> Double
+     (Eq word, Eq cls, Hashable cls)
+  => Env cls word
+  -> Document cls word
+  -> Double
 normalizationFactor env d = sum (map f (envClasses env))
   where
     f c = classPrior env c * documentProbability env d c
 
 -- | Pr(c|t_i). Main working horse.
-multinomialNaiveBayes :: (Eq word, Eq cls) => Env cls word -> [word] -> cls
+multinomialNaiveBayes ::
+     (Eq word, Eq cls, Hashable cls) => Env cls word -> [word] -> cls
 multinomialNaiveBayes env wrds = getMax (map f (envClasses env))
   where
     d = Document (error "classes not known yet") wrds
@@ -108,17 +138,22 @@ multinomialNaiveBayes env wrds = getMax (map f (envClasses env))
 
 -- | transforms original word's frequency
 tfidf ::
-     (Eq word, Eq cls) => Env cls word -> Document cls word -> word -> Double
+     (Eq word, Hashable word, Eq cls)
+  => Env cls word
+  -> Document cls word
+  -> word
+  -> Double
 tfidf env doc word =
   log (countWordInDoc word doc + 1) *
-  log (totalNumberOfDocs env / dlength (docsWithWord env word))
+  log (totalNumberOfDocs env / countDocsWithWord env word)
 
 -- | All classes but the one in the argument
 complementClasses :: Eq cls => Env cls word -> cls -> [cls]
 complementClasses env c = filter (/= c) (envClasses env)
 
 -- | w_n_c
-wordWeight :: (Eq word, Eq cls) => Env cls word -> word -> cls -> Double
+wordWeight ::
+     (Eq word, Eq cls, Hashable cls) => Env cls word -> word -> cls -> Double
 wordWeight env word cls = log ((1 + s1) / vocabularySize env + s2)
   where
     s1 = sum (map s1f (complementClasses env cls))
@@ -129,7 +164,7 @@ wordWeight env word cls = log ((1 + s1) / vocabularySize env + s2)
 
 -- | TWCNB
 transformedWeightNormalizedComplementNaiveBayes ::
-     (Eq word, Eq cls) => Env cls word -> [word] -> cls
+     (Eq word, Eq cls, Hashable cls) => Env cls word -> [word] -> cls
 transformedWeightNormalizedComplementNaiveBayes env wrds =
   getMin (map f (envClasses env))
   where
@@ -139,18 +174,42 @@ transformedWeightNormalizedComplementNaiveBayes env wrds =
     getMin = fst . head . sortBy (compare `on` snd)
 
 -- | The "training" step
-buildEnv :: (Eq word, Eq cls) => [Document cls word] -> Env cls word
-buildEnv docs = Env classes docs voc
+buildEnv ::
+     (Eq word, Hashable word, Eq cls, Hashable cls)
+  => [Document cls word]
+  -> Env cls word
+buildEnv docs =
+  Env
+    classes
+    docs
+    voc
+    (EfficiencyCache
+       docsPerClass'
+       docsWithWordNum'
+       docsTotalNumber'
+       effVocabularySize')
   where
     classes = nub (concatMap docClasses docs)
     voc = nub (concatMap docWords docs)
+    docsPerClass' =
+      H.fromListWith
+        (++)
+        (concatMap
+           (\doc -> concatMap (\c -> [(c, [doc])]) (docClasses doc))
+           docs)
+    docsWithWordNum' =
+      H.fromListWith
+        (+)
+        (concatMap (\doc -> concatMap (\w -> [(w, 1)]) (docWords doc)) docs)
+    docsTotalNumber' = dlength docs
+    effVocabularySize' = dlength voc
 
 -- ** Animals example from http://dataaspirant.com/2017/02/06/naive-bayes-classifier-machine-learning/
 data AnimalClass
   = Parrot
   | Dog
   | Fish
-  deriving (Eq, Generic, Show)
+  deriving (Eq, Generic, Show, Hashable)
 
 instance J.FromJSON AnimalClass
 
@@ -159,7 +218,7 @@ data AnimalFeatures
   | Wings
   | GreenColor
   | DangerousTeeth
-  deriving (Eq, Generic, Show)
+  deriving (Eq, Generic, Show, Hashable)
 
 instance J.FromJSON AnimalFeatures
 
